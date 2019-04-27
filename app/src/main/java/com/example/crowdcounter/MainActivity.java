fed8e99af59648bd7e041d8d559c3b1e11285c86
ocollin1 <ocollin1@binghamton.edu>
2019-04-26 22:29:58 -0400

    Final Commit

--- app/src/main/java/com/example/crowdcounter/MainActivity.java
+++ app/src/main/java/com/example/crowdcounter/MainActivity.java
@@ -17,6 +17,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -24,15 +26,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextInputEditText guessField = (TextInputEditText) findViewById(R.id.GuessField);
+        final TextInputEditText nameField = (TextInputEditText) findViewById(R.id.NameField);
         final Button submitButton = (Button) findViewById(R.id.button);
         final TextView mainText = (TextView) findViewById(R.id.main);
 
+        nameField.setHint("Name Here..");
         guessField.setHint("Guess Here..");
 
         final RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         String url ="http://cs.binghamton.edu/~rburton2/gettext.php";
 
         // Request a string response from the provided URL.
+        /*
         final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
@@ -43,7 +48,12 @@ public class MainActivity extends AppCompatActivity {
                         if(response.length()<500) {
                             maxL = response.length();
                         }
-                        mainText.setText(response.substring(0,maxL));
+
+                        String result = response.substring(0,maxL);
+                        if(){
+
+                        }
+                        mainText.setText();
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -55,15 +65,59 @@ public class MainActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         //queue.add(stringRequest);
+        */
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     //setContentView(R.layout.answer);
-                    String guess = guessField.getText().toString();
+                    final String guess = guessField.getText().toString();
                     int value = Integer.parseInt(guess);
-                    queue.add(stringRequest);
+
+                    String name = nameField.getText().toString().replaceAll(" ", "_");
+
+                    if(name.equals("")){
+                        name = "Anonymous";
+                    }
+
+                    String url ="http://cs.binghamton.edu/~rburton2/sendguess.php?guess=" + guess + "&name=" + name;
+
+                    System.out.println(url);
+                    //mainText.setText(url);
+
+                    // Request a string response from the provided URL.
+                    final StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    // Display the first 500 characters of the response string.
+                                    System.out.print(response);
+                                    int maxL = 500;
+                                    if(response.length()<500) {
+                                        maxL = response.length();
+                                    }
+
+                                    String result = response.substring(0,1);
+                                    System.out.println(result);
+                                    if(result.equals("0")){
+                                        mainText.setText("Sorry, but " + guess + " is incorrect!");
+
+                                    } else {
+                                        setContentView(R.layout.answer);
+                                    }
+                                    //mainText.setText(response.substring(0,maxL));
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            mainText.setText("That didn't work!");
+                            System.out.println(error);
+                        }
+                    });
+
+
+                    queue.add(getRequest);
                     guessField.setHint(guess);
 
                 } catch (Exception e){
