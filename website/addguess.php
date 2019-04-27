366c19dd817ec4605a0909360a06fd878a9e8d7d
commontone <robert@robert.red>
2019-04-26 20:40:37 -0400

    More site changes

--- website/addguess.php
+++ website/addguess.php
@@ -2,5 +2,6 @@
 	$mysqli = mysqli_connect("mysql.cs.binghamton.edu", "rburton2","ahchaich9Hoh","rburton2_cs441");
 	$sql = "INSERT INTO entries (event, guess, name, email) VALUES(1, 20,'John Brown','example@example.com');";
 	$result = mysqli_query($mysqli, $sql);
+	mysql_close($mysqli);
 	echo $result;
 ?>
